1996a167a77049b07dab0138c644fd15bd4d1882
ashikfifa <ashikahmedfifa@gmail.com>
2025-03-28 15:30:47 +0600

    main layout

--- src/layout/main-layout.tsx
+++ src/layout/main-layout.tsx
@@ -1,14 +1,25 @@
 import { ReactNode } from "react";
-
-import { Montserrat } from "next/font/google";
+import localFont from "next/font/local";
 
 import Navbar from "@/layout/navbar";
 import Footer from "@/layout/footer";
 import { routes } from "@/data/navigationRoutes";
 import { classNames } from "@/utility/classNames";
 
-const montserrat = Montserrat({
-  subsets: ["latin"],
+const montserrat = localFont({
+  src: [
+    {
+      path: "/fonts/montserrat/Montserrat-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "/fonts/montserrat/Montserrat-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  variable: "--font-montserrat",
 });
 
 export interface MainLayoutProps {
