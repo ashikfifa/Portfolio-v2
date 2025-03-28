c8c478c366506f0c716aafbeea3a444f7763bb23
ashikfifa <ashikahmedfifa@gmail.com>
2025-03-28 20:47:29 +0600

    date

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -23,7 +23,7 @@ export default function Footer() {
         </div>
       </div>
       <div className="flex w-full flex-col items-center justify-between gap-8 text-center  md:flex-row md:justify-between lg:mx-auto lg:max-w-7xl">
-        <span className="text-foreground">©2024 Md Asiquzzaman</span>
+        <span className="text-foreground">©2025 Md Asiquzzaman</span>
         <div className="flex gap-8">
           <a
             href={siteMetadata.github}
